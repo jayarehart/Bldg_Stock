3ea4566e0bfde5f55c665a427cf5538c128cc607
Jay Arehart <joseph.arehart@colorado.edu>
2020-04-06 19:54:56 +0100

    added FA elasticity from EDGE model (real values)

--- Scripts/dsm_res.py
+++ Scripts/dsm_res.py
@@ -9,6 +9,7 @@ from odym import dynamic_stock_model as dsm
 
 # Load in datasets
 data_pop = pd.read_excel('./InputData/Pop_Data.xlsx', sheet_name='USA_pop_forecast')
+data_gdp = pd.read_excel('./InputData/Pop_Data.xlsx', sheet_name='GDP_pc')
 RECS_Weights = pd.read_excel('./InputData/Pop_Data.xlsx', sheet_name='res_weight')
 FA_elas_res = pd.read_excel('./InputData/Pop_Data.xlsx', sheet_name='FA_elas_res')  # dummy data for now
 
@@ -64,6 +65,62 @@ def interpolate_population(data_pop, year1=1900, year2=2100, proj='median', kind
 
     return years, US_pop_years
 
+def interpolate_gdp(data_gdp, year1=1900, year2=2100, SSP='SSP1', kind='cubic', plot=True):
+    """ Interpolate the GDP data between the specified years for the US.
+        Choose a UN projection for future population data (median, upper_95, lower_95, upper_80, or lower_80).
+        Options for 'kind' are [linear, cubic, nearest, previous, and next] """
+
+    # Create interpolations for population
+    f_SSP1 = interp1d(data_gdp.Year, data_gdp.SSP1, kind=kind)
+    f_SSP2 = interp1d(data_gdp.Year, data_gdp.SSP2, kind=kind)
+    f_SSP3 = interp1d(data_gdp.Year, data_gdp.SSP3, kind=kind)
+    f_SSP4 = interp1d(data_gdp.Year, data_gdp.SSP4, kind=kind)
+    f_SSP5 = interp1d(data_gdp.Year, data_gdp.SSP5, kind=kind)
+
+    # Study Period
+    # year1 = 1900
+    # year2 = 2100
+    years = np.linspace(year1, year2, num=(year2 - year1 + 1), endpoint=True)
+
+    if SSP == 'SSP1':
+        US_gdp = f_SSP1(years)
+    elif SSP == 'SSP2':
+        US_gdp = f_SSP2(years)
+    elif SSP == 'SSP3':
+        US_gdp = f_SSP3(years)
+    elif SSP == 'SSP4':
+        US_gdp = f_SSP4(years)
+    elif SSP == 'SSP5':
+        US_gdp = f_SSP5(years)
+    elif SSP == 'All':
+        US_gdp = pd.DataFrame({'gdp_SSP1': f_SSP1(years),
+                               'gdp_SSP2': f_SSP2(years),
+                               'gdp_SSP3': f_SSP3(years),
+                               'gdp_SSP4': f_SSP4(years),
+                               'gdp_SSP5': f_SSP5(years)})
+    else:
+        US_gdp = None
+    years_df = pd.DataFrame({'Year': years})
+    US_gdp_years = pd.concat([years_df, US_gdp], axis=1)
+
+    if plot == True:
+        # Plot of population forecasts
+        plt1, = plt.plot(years, f_SSP1(years))
+        plt2, = plt.plot(years, f_SSP2(years))
+        plt3, = plt.plot(years, f_SSP3(years))
+        plt4, = plt.plot(years, f_SSP4(years))
+        plt5, = plt.plot(years, f_SSP5(years))
+        # plt6, = plt.plot([base_year, base_year], [2.4e8, 4.5e8], color='k', LineStyle='--')
+        plt.legend([plt1, plt2, plt3, plt4, plt5],
+                   ['SSP1', 'SSP2', 'SSP3', 'SSP4', 'SSP5'],
+                   loc=2)
+        plt.xlabel('Year')
+        plt.ylabel('US GDP per capita')
+        plt.title('Historical and Forecast of per-capita GDP in the US')
+        plt.show();
+
+    return US_gdp_years
+
 
 def FA_elasticity_linear(year1=1900, year2=2100, base_year=2016,
                          FA_elas_year1=100, FA_elas_base_year=200, FA_elas_year2=200,
@@ -89,6 +146,100 @@ def FA_elasticity_linear(year1=1900, year2=2100, base_year=2016,
         plt.show();
     return FA_elas
 
+def FA_elasticity_EDGE(US_gdp, US_pop, SSP='SSP1',
+                       base_year=2016,FA_base_year=347, Area_country=9.14759e6, gamma=-0.03,
+                       plot=True):
+    """ Area of the USA is 9.834 million km².
+        Base year floor are elasticity for all buildings is 347 m2/person as determined by article (in review)"""
+
+    def calc_FA_elas(gdp, SSP, SSP_split_year=1985):
+        # Beta values for each SSP
+        Beta_SSP = {'SSP1': 0.3,
+                    'SSP2': 0.7,
+                    'SSP3': 0.8,
+                    'SSP4': 0.7,
+                    'SSP5': 1.0}
+        # General Beta for floor space demand:
+        Beta = 0.42
+
+        FA_df = pd.merge(US_pop, gdp, on='Year')
+        FA_df = FA_df.set_index('Year', drop=False)
+
+        # calculate historical FA
+        FA_df['Pop_Dens'] = FA_df['US_pop'] / Area_country
+        base_year_0_df = FA_df.loc[[SSP_split_year]]
+        alpha  = FA_base_year / (base_year_0_df['gdp_'+SSP] ** Beta * base_year_0_df['Pop_Dens'] ** gamma)
+        # alpha reported by EDGE model is 0.61.
+        # alpha from Arehart et al. 2020 high   = 5.002223
+        #                                median = 4.350305
+        #                                low    = 3.635701
+
+
+
+
+        FA_df.loc[FA_df['Year'] <= SSP_split_year, 'FA_elas_'+SSP] = alpha[SSP_split_year] * (FA_df['gdp_'+SSP] ** Beta) * FA_df['Pop_Dens'] ** gamma
+        for i in range(1,len(FA_df)):
+            year_i = FA_df.index[i]
+            if year_i > SSP_split_year:
+                row_t_1 = FA_df.loc[year_i-1]
+                FA_t_1 = row_t_1['FA_elas_'+SSP]
+                I_t_1 = row_t_1['gdp_'+SSP]
+                D_t_1 = row_t_1['Pop_Dens']
+                row_t = FA_df.loc[year_i]
+                I_t = row_t['gdp_'+SSP]
+                D_t = row_t['Pop_Dens']
+                FA_df.loc[int(year_i),'FA_elas_'+SSP] = FA_t_1 * (I_t/I_t_1) ** Beta_SSP[SSP] * (D_t/D_t_1) ** gamma
+        return FA_df
+
+    FA_SSP1 = calc_FA_elas(US_gdp, 'SSP1')
+    FA_SSP2 = calc_FA_elas(US_gdp, 'SSP2')
+    FA_SSP3 = calc_FA_elas(US_gdp, 'SSP3')
+    FA_SSP4 = calc_FA_elas(US_gdp, 'SSP4')
+    FA_SSP5 = calc_FA_elas(US_gdp, 'SSP5')
+
+    if SSP=='SSP1':
+        df_return = FA_SSP1
+    elif SSP=='SSP2':
+        df_return = FA_SSP2
+    elif SSP=='SSP3':
+        df_return = FA_SSP3
+    elif SSP=='SSP4':
+        df_return = FA_SSP4
+    elif SSP=='SSP5':
+        df_return = FA_SSP5
+    elif SSP=='All':
+        df_return = pd.DataFrame({'FA_SSP1': FA_SSP1['FA_elas_SSP1'],
+                                  'FA_SSP2': FA_SSP2['FA_elas_SSP2'],
+                                  'FA_SSP3': FA_SSP3['FA_elas_SSP3'],
+                                  'FA_SSP4': FA_SSP4['FA_elas_SSP4'],
+                                  'FA_SSP5': FA_SSP5['FA_elas_SSP5'], })
+
+        if plot == True:
+            plt1, = plt.plot(df_return.index, df_return.FA_SSP1)
+            plt2, = plt.plot(df_return.index, df_return.FA_SSP2)
+            plt3, = plt.plot(df_return.index, df_return.FA_SSP3)
+            plt4, = plt.plot(df_return.index, df_return.FA_SSP4)
+            plt5, = plt.plot(df_return.index, df_return.FA_SSP5)
+            plt6, = plt.plot([base_year, base_year], [0, 800], color='k', LineStyle='--')
+            plt.legend([plt1, plt2, plt3, plt4, plt5],
+                       ['SSP1', 'SSP2', 'SSP3', 'SSP4', 'SSP5'], loc=2)
+            plt.xlabel('Year')
+            plt.ylabel('Floor Area Elasticity')
+            plt.title('Floor Area Elasticity for various SSPs')
+            plt.show();
+
+
+    return df_return
+
+
+    # FA_historic = np.linspace(FA_elas_year1, FA_elas_base_year, num=(base_year - year1 + 1), endpoint=True)
+    # FA_future = np.linspace(FA_elas_base_year, FA_elas_year2, num=(year2 - base_year), endpoint=True)
+    #
+    # FA_elas = pd.DataFrame({'Year': years,
+    #                         'FA_elas': np.concatenate((FA_historic, FA_future), axis=0)
+    #                         }, )
+
+
 # def FA_elasticity_interp(year1=1900, year2=2100, x_years, y_FA_elas):
 #     x_years =
 #
@@ -96,15 +247,26 @@ def FA_elasticity_linear(year1=1900, year2=2100, base_year=2016,
 # Time period input variables
 year1 = 1900
 year2 = 2100
-base_year = 2015
+base_year = 2016
 
 # interpolate population data for the US.
 years, US_pop = interpolate_population(data_pop, year1=year1, year2=year2, proj='median', plot=False)
 
+# interpolate gdp data for the US.
+US_gdp = interpolate_gdp(data_gdp, year1=1900, year2=2100, SSP='All', kind='cubic', plot=True)
+# US_gdp_all =
+
+# calculate total floor area elasticity
+FA_all = FA_elasticity_EDGE(US_gdp, US_pop, SSP='All',
+                       base_year=2016,FA_base_year=347, Area_country=9.14759e6, gamma=-0.03,
+                       plot=True)
+
+
+
 # interpolate floor area elasticity for each year
-FA_elas_res = FA_elasticity_linear(year1=year1, year2=year2, base_year=base_year,
-                                   FA_elas_year1=100, FA_elas_base_year=100, FA_elas_year2=100,
-                                   plot=False, plot_name='Residential')
+# FA_elas_res = FA_elasticity_linear(year1=year1, year2=year2, base_year=base_year,
+#                                    FA_elas_year1=100, FA_elas_base_year=100, FA_elas_year2=100,
+#                                    plot=True, plot_name='Residential')
 
 # calculate demanded floor area stock
 FA_stock_res = np.multiply(US_pop.US_pop, FA_elas_res.FA_elas) / 1000000
@@ -127,7 +289,7 @@ lifetime_WeibullLT = {'Type': 'Weibull', 'Shape': np.array([5.5]), 'Scale': np.a
 # lifetime_WeibullLT = {'Type': 'Weibull', 'Shape': np.array([1.47]), 'Scale': np.array([37.64])}
 
 # Gamma (currently not working)
-# lifetime_GammaLT = {'Type': 'Gamma', 'Scale': np.array([2.7]), 'Shape': np.array([32.9])}
+lifetime_GammaLT = {'Type': 'Gamma', 'Scale': np.array([2.7]), 'Shape': np.array([32.9])}
 
 # Lognormal (currently not working)
 lifetime_LogNormalLT = {'Type': 'LogNormal', 'Mean': np.array([50]), 'StdDev': np.array([10])}
@@ -135,10 +297,10 @@ lifetime_LogNormalLT = {'Type': 'LogNormal', 'Mean': np.array([50]), 'StdDev': n
 # Fixed lifetime
 lifetime_FixedLT = {'Type': 'Fixed', 'Mean': np.array([BldgLife_mean] * len(years))}
 
-
 # Residential floor area  age-cohort in base year: 2015
 S_0_res_2015 = np.flipud(RECS_Weights.Res_Weight * FA_stock_res[list(years).index(base_year)])
 
+# Function to call the dynamic stock model.
 def do_stock_driven_model(t, s, lt, InitialStock, SwitchTime, plot=True):
     """ Compute a stock driven model from an initial stock.
         Returns an object with class dynamic_stock_model with age-cohort matrix
@@ -189,16 +351,28 @@ def do_stock_driven_model(t, s, lt, InitialStock, SwitchTime, plot=True):
 
     return my_dsm
 
+# Calculate a dynamic stock model
 t = years
 s = FA_stock_res
-lt = lifetime_NormalLT
+lt = lifetime_LogNormalLT
 InitialStock=S_0_res_2015
-SwitchTime=116
-US_stock_res = do_stock_driven_model(t, s, lt, InitialStock, SwitchTime, plot=False)
+SwitchTime=list(years).index(base_year + 1)     # 116
+US_stock_res = do_stock_driven_model(t, s, lt, InitialStock, SwitchTime, plot=True)
 
-# 'compute_evolution_initialstock':
+
+
+
+
+
+
+
+
+
+
 
 # ----------------------------------------------------------------------------------------------------------------------
+# OLD CODE
+#
 # 'compute_stock_driven_model_initialstock
 # BldgLife_mean = 120  # years
 # Normal_StdDev = 0.6 *  np.array([BldgLife_mean] * len(years))
